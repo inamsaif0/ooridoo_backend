7fce5356117356f177170e8e614ab4211fbbaf14
inamsaif0 <inamsaif586@gmail.com>
2024-08-10 08:27:41 -0700

    empy

--- validations/userValidation.js
+++ validations/userValidation.js
@@ -78,6 +78,7 @@ exports.productValidation = Joi.object({
   title: Joi.string().required(),
   description: Joi.string().required(),
   productType: Joi.string().required(),
+  sku: Joi.string().allow(null, ''),
   brandName: Joi.string().allow(null),
   category: Joi.string().allow(null),
   price: Joi.number().allow(null)
